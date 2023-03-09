9af10daf263b5c130cc0bc61e48f0f0cd5039333
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-03-09 12:48:18 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -15,8 +15,8 @@ terraform {
 # }
 provider "azurerm" {
   features {}
-  subscription_id   = "3f87c349-6fa2-455b-bd5b-1222f6d15367"
-  tenant_id         = "037f8e6b-2776-49f6-bd0f-234af6a8030b"
-  client_id         = "17d948e8-5a27-499f-a64c-ca6481ec5439"
-  client_secret     = "kSe8Q~aH.~E4En0gmX2NxQ13_7oFJ7SX-w-kGbUk"
+  subscription_id   = var.subscriptionId
+  tenant_id         = var.tenantId
+  client_id         = var.clientId
+  client_secret     = var.clientSecret
 }
