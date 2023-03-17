5093aee83a4604f88836326f65a61e28c39627a1
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-03-17 18:20:56 +0000

    Create

---
+++ tfvars.tf
@@ -0,0 +1,8 @@
+subscriptionId                = "3f87c349-6fa2-455b-bd5b-1222f6d15367"
+tenantId                      = "037f8e6b-2776-49f6-bd0f-234af6a8030b"
+clientId                      = "17d948e8-5a27-499f-a64c-ca6481ec5439"
+clientSecret                  = "kSe8Q~aH.~E4En0gmX2NxQ13_7oFJ7SX-w-kGbUk"
+root_id                       = "csc-123"
+root_name                     = "csc-team"
+security_alerts_email_address = "cuong.dq@csc-jsc.com"
+management_resources_location = "eastus"
