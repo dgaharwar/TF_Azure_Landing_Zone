7008ec9b1987eea6a6d1e208d2c524508a531865
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-03-17 18:22:42 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "log_retention_in_days" {
 
 variable "security_alerts_email_address" {
   type    = string
-  default = "deeps12luv@gmail.com"
+  default = "cuong.dq@csc-jsc.com"
 }
 
 variable "management_resources_location" {
